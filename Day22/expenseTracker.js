61c0e04321ce50aad88125dcc6a2034c835d73ea
meghasaini18 <meghasaini1784@gmail.com>
2025-06-10 21:52:30 +0530

    add Day22

---
+++ Day22/expenseTracker.js
@@ -0,0 +1,7 @@
+const balance = document.getElementsByClassName("balance");
+const moneyPlus = document.getElementsByClassName("money-plus");
+const moneyMinus = document.getElementsByClassName("money-minus");
+const list = document.getElementsByClassName("list");
+const form = document.getElementsByClassName("form");
+const text = document.getElementsByClassName("text");
+const amount = document.getElementsByClassName("amount");
